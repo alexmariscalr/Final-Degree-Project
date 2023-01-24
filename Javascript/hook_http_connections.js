a1f0ed2970d38ff053c7ccedb4da333db64553c6
Alejandro Mariscal Romero <amariscalr@indra.es>
2023-01-24 21:19:52 +0100

    detecting local and remote host and port

--- Javascript/hook_http_connections.js
+++ Javascript/hook_http_connections.js
@@ -1,57 +1,106 @@
+
 // Function to be called when a connection is made
 setImmediate(function () {
   console.log("[*] Starting script");
   Java.perform(function () {
     var HttpURLConnection = Java.use("java.net.HttpURLConnection");
-    var URL = Java.use("java.net.URL");
+    //var URL = Java.use("java.net.URL");
     var HttpsURLConnection = Java.use("javax.net.ssl.HttpsURLConnection");
-    var OkHttpClient = Java.use("okhttp3.OkHttpClient");
+    // var OkHttpClient = Java.use("okhttp3.OkHttpClient");
     var Thread = Java.use("java.lang.Thread")
     
     //API Android
-    
-    URL.openConnection.overload().implementation = function () {
-      var connection = this.openConnection(); //URLConnection
-      var headers = connection.getRequestProperties(); //Map<String, List<String>>
-      var stackTrace = Thread.currentThread().getStackTrace()
-      console.log("1" + stackTrace)
-      console.log( "\n[URL.openConnection()] Opening HTTP/HTTPS connection to: " + connection.getURL().toString() );
-      return connection;
-    };
 
-    URL.openConnection.overload("java.net.Proxy").implementation = function ( proxy ) {
-      var URLConnection = this.openConnection(proxy);
-      console.log( "\n[URL.openConnection with proxy] Opening HTTP/HTTPS connection to: " + connection.getURL().toString() );
-      return URLConnection;
-    };
+    //Class URLConnection
+    var URLConnection = Java.use("java.net.URLConnection")
+    URLConnection.connect.implementation = function() {
+      var requestMethod = this.getClass().getDeclaredField("requestMethod");
+      requestMethod.setAccesible(true);
+      requestMethod = requestMethod.get(this)
+      var connection = ''
 
-    var HttpURLConnection = Java.use('java.net.HttpURLConnection');
+      send('{"type":"URLConnection.connect()' + 
+      '","url":"' + this.url.toString() +
+      '","port":"' + this.url.getPort() +
+      '","requestMethod":"' + requestMethod +
+      '","connection":"' + connection +
+      '","stackTrace:"'+ Thread.currentThread().getStackTrace() +
+      '","domain:"N/A'+ 
+      '","thirdParties":"'+ findThirdParties() +
+      '","event":"intercept","status":true}')  
+      return this.connect();
+    }   
+
+    //Class HttpURLConnection (llama internamente a URLConnection.connect())
+     var HttpURLConnection = Java.use('java.net.HttpURLConnection');
     HttpURLConnection.connect.implementation = function () {   
-        console.log("\n[HttpURLConnection.connect] Opening HTTP/HTTPS connection to: " + this.getURL());
-        console.log("[OkHttpClient.newCall] Using method: " + this.getRequestMethod());
+      console.log(" \n")
+        send('{"type":" HttpURLConnection.connect()' + 
+        '","url":"' + this.getURL().toString() +
+        '","port":"' + this.getURL().getPort() +
+        '","requestMethod":"' + this.getRequestMethod() +
+        '","stackTrace:"'+ Thread.currentThread().getStackTrace() +
+        '","domain:"N/A'+ 
+        '","event":"intercept","status":true}')  
         return this.connect();
     };
-
-    HttpsURLConnection.connect.implementation = function () {
-      console.log( "\n[HttpsURLConnection.connect] Opening HTTP/HTTPS connection to: " + this.getURL().toString() );
-      console.log( "[HttpsURLConnection.connect] Using method: " + this.getRequestMethod() );
-      this.connect();
+ 
+    //Class HttpsURLConnection (llama internamente a URLConnection.connect())
+     HttpsURLConnection.connect.implementation = function () {
+      console.log(" \n")
+      send('{"type":" HttpURLConnection.connect()' + 
+      '","url":"' + this.url.toString() +
+      '","port":"' + this.url.getPort() +
+      '","requestMethod":"' + this.getRequestMethod() +
+      '","stackTrace:"'+ Thread.currentThread().getStackTrace() +
+      '","domain:"N/A'+ 
+      '","event":"intercept","status":true}')  
+      return this.connect();
+  };
+ 
+  //Class Socket (API Android)
+    var Socket = Java.use('java.net.Socket');
+    Socket.connect.overload('java.net.SocketAddress').implementation = function (endpoint) {
+      console.log(" \n")
+      send('{"type":" Socket.connect(endpoint)' +
+      '","stackTrace:"'+ Thread.currentThread().getStackTrace() +
+      '","domain:"N/A'+ 
+      '","event":"intercept","status":true}')  
+        return this.connect(endpoint);
     };
 
-    var Socket = Java.use('java.net.Socket');
-    Socket.connect.overload('java.net.SocketAddress', 'int').implementation = function (address, timeout) {
-      var stackTrace = Thread.currentThread().getStackTrace()
-      console.log("2" + stackTrace)
-        console.log("\n[Socket.connect] Opening HTTP/HTTPS connection to: " + address); 
-        return this.connect(address, timeout);
+    Socket.connect.overload('java.net.SocketAddress', 'int').implementation = function (endpoint, timeout) {
+      console.log("\n")
+      send('{"type":" Socket.connect(endpoint, timeout)' + 
+      '","stackTrace:"'+ Thread.currentThread().getStackTrace() +
+      '","domain:"N/A'+ 
+      '","event":"intercept","status":true}')  
+        return this.connect(endpoint, timeout);
+    };
+
+    Socket.getOutputStream.implementation = function () {
+      var lPort = this.getLocalPort();
+      var lAddr = this.getLocalSocketAddress();
+      var rPort = this.getPort();
+      var rAddr = this.getRemoteSocketAddress();
+      console.log("\n")
+      send('{"type":" Socket.getOutputStream()' + 
+      '","local address":"' + lAddr +
+      '","local port":"' + lPort +
+      '","remote address":"' + rAddr +
+      '","remote port":"' + rPort +
+      '","stackTrace:"'+ Thread.currentThread().getStackTrace() +
+      '","domain:"N/A'+ 
+      '","event":"intercept","status":true}')  
+        return this.getOutputStream();
     };
 
 
     //Square API 
-    OkHttpClient.newCall.overload("okhttp3.Request").implementation = function ( request ) {
+/*     OkHttpClient.newCall.overload("okhttp3.Request").implementation = function ( request ) {
       console.log( "\n[OkHttpClient.newCall] Opening HTTP/HTTPS connection to: " + request.url().toString() );
       console.log("[OkHttpClient.newCall] Using method: " + request.method());
       return this.newCall(request);
-    };
+    }; */
   });
 });
